883166ca44f06bcb91da4b3333fbafcdcdcc7c8e
fbc299 <fbc299@vip.qq.com>
2026-04-29 23:15:48 +0800

    feat: add WorkerProvider

---
+++ lib/providers/worker_provider.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../models/worker.dart';
+import '../database/worker_dao.dart';
+
+class WorkerProvider extends ChangeNotifier {
+  final WorkerDao _dao = WorkerDao();
+  List<Worker> _workers = [];
+  bool _isLoading = false;
+
+  List<Worker> get workers => _workers;
+  bool get isLoading => _isLoading;
+
+  Future<void> loadAll() async {
+    _isLoading = true;
+    notifyListeners();
+    _workers = await _dao.getAll();
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> addWorker(Worker worker) async {
+    await _dao.insert(worker);
+    await loadAll();
+  }
+
+  Future<void> updateWorker(Worker worker) async {
+    await _dao.update(worker);
+    await loadAll();
+  }
+
+  Future<void> deleteWorker(int id) async {
+    await _dao.delete(id);
+    await loadAll();
+  }
+}
