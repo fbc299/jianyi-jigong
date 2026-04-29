df3f1383d521d9eeb64a3205ecbca83a64a92a53
fbc299 <fbc299@vip.qq.com>
2026-04-29 23:18:52 +0800

    feat: register ProjectProvider and WorkerProvider

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:provider/provider.dart';
 import 'app.dart';
 import 'providers/work_provider.dart';
 import 'providers/salary_provider.dart';
+import 'providers/project_provider.dart';
+import 'providers/worker_provider.dart';
 import 'database/database_helper.dart';
 
 void main() async {
@@ -13,6 +15,8 @@ void main() async {
       providers: [
         ChangeNotifierProvider(create: (_) => WorkProvider()),
         ChangeNotifierProvider(create: (_) => SalaryProvider()),
+        ChangeNotifierProvider(create: (_) => ProjectProvider()),
+        ChangeNotifierProvider(create: (_) => WorkerProvider()),
       ],
       child: const JianYiApp(),
     ),
