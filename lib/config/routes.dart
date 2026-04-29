24eb23bb5ac0381430994b234883b8eb46e813d8
fbc299 <fbc299@vip.qq.com>
2026-04-29 23:18:50 +0800

    feat: add project/worker routes

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -7,6 +7,10 @@ import '../screens/salary/salary_list.dart';
 import '../screens/salary/salary_form.dart';
 import '../screens/stats/stats_screen.dart';
 import '../screens/settings/settings_screen.dart';
+import '../screens/project/project_list.dart';
+import '../screens/project/project_form.dart';
+import '../screens/worker/worker_list.dart';
+import '../screens/worker/worker_form.dart';
 
 class AppRoutes {
   static const String home = '/';
@@ -16,6 +20,10 @@ class AppRoutes {
   static const String salaryForm = '/salary/form';
   static const String stats = '/stats';
   static const String settings = '/settings';
+  static const String projectList = '/project/list';
+  static const String projectForm = '/project/form';
+  static const String workerList = '/worker/list';
+  static const String workerForm = '/worker/form';
 
   static Map<String, WidgetBuilder> get routes => {
         home: (context) => const MainScreen(),
@@ -25,5 +33,9 @@ class AppRoutes {
         salaryForm: (context) => const SalaryFormScreen(),
         stats: (context) => const StatsScreen(),
         settings: (context) => const SettingsScreen(),
+        projectList: (context) => const ProjectListScreen(),
+        projectForm: (context) => const ProjectFormScreen(),
+        workerList: (context) => const WorkerListScreen(),
+        workerForm: (context) => const WorkerFormScreen(),
       };
 }
